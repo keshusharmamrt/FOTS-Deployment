844c7f7e6399eba8cb16b8b34b4bc31838d2562d
keshusharmamrt <69103575+keshusharmamrt@users.noreply.github.com>
2021-01-16 20:04:39 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,15 +1,5 @@
 import streamlit as st
-import numpy as np
-import cv2
-import numpy as np
-import pandas as pd
-import os
-import math
-import cv2
-import time
-import numpy as np
-from shapely.geometry import Polygon
-from PIL import Image
+import tensorflow as tf
 def main():
   st.title("FOTS: Scene Text Parsing")
 if __name__ == '__main__':
